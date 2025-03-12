c3c38a253b9032efc0354cbe04ab1431c13883aa
Chaima IDOUDI <cidoudi@sofrecom.fr>
2025-03-12 15:38:52 +0100

    add backend project

---
+++ Backend/Dynamics365_Backend/Dynamics365_Backend/Models/UserCredentials.cs
@@ -0,0 +1,8 @@
+﻿namespace Dynamics365_Backend.Models
+{
+    public class UserCredentials
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Backend/Dynamics365_Backend/Dynamics365_Backend/Services/ConncetionCRM_Service.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.PowerPlatform.Dataverse.Client;
+using System;
+namespace Dynamics365_Backend.Services
+{
+    public class ConncetionCRM_Service
+    {
+        public ServiceClient ConnectToDynamic365 (string email, string password)
+        {
+            
+        }
+    }
+}
