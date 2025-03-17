e604c860f4835d77212e63de8bfcd0a4d47c1d3e
Chaima IDOUDI <cidoudi@sofrecom.fr>
2025-03-17 12:31:56 +0100

    adjust password visibility

--- Frontend/Dynamics365_Frontend/src/app/app.component.ts
+++ Frontend/Dynamics365_Frontend/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule,LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Dynamics365_Frontend';
+  
 }
